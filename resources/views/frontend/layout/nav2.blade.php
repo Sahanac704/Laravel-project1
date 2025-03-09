35ebb9af1fc04a0ec4742f0e7e59ccf152d167ec
Sahana <chaudharysahana5@gmail.com>
2025-03-09 12:29:46 +0545

    upload

---
+++ resources/views/frontend/layout/nav2.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-light bg-white">
+    <div class="container-fluid mx-5">
+      <a class="navbar-brand">eStore</a>
+      <form class="d-flex mx-auto" role="search">
+        <input class="form-control px-5 me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
+      <div class="icon">
+      <ion-icon name="person-outline"></ion-icon>
+      <ion-icon name="heart-outline"></ion-icon>
+      <ion-icon name="cart-outline"></ion-icon>
+      </div>
+
+    </div>
+  </nav>
---
+++ resources/views/frontend/layout/nav3.blade.php
@@ -0,0 +1,70 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white mx-auto">
+    <div class="container-fluid border p-2">
+
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav mx-5">
+          <li class="nav-item drop">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item drop">
+            <a class="nav-link active" href="#">About</a>
+          </li>
+          <li class="nav-item drop">
+            <a class="nav-link active" href="#">Category</a>
+          </li>
+          <li class="nav-item drop">
+            <a class="nav-link active" href="#">Product Details</a>
+          </li>
+          <li class="nav-item drop">
+            <a class="nav-link active" href="#">Cart</a>
+          </li>
+          <li class="nav-item drop">
+            <a class="nav-link active" href="#">Checkout</a>
+          </li>
+          <li class="nav-item dropdown drop">
+            <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">Dropdown 1</a></li>
+              <li><a class="dropdown-item" href="#">Deep Dropdown</a></li>
+              <li><a class="dropdown-item" href="#">Dropdown 2</a></li>
+              <li><a class="dropdown-item" href="#">Dropdown 3</a></li>
+              <li><a class="dropdown-item" href="#">Dropdown 4</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown drop">
+            <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Megamenu 1
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">Dropdown 1</a></li>
+              <li><a class="dropdown-item" href="#">Deep Dropdown</a></li>
+              <li><a class="dropdown-item" href="#">Dropdown 2</a></li>
+              <li><a class="dropdown-item" href="#">Dropdown 3</a></li>
+              <li><a class="dropdown-item" href="#">Dropdown 4</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item dropdown drop">
+            <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Megamenu 2
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">Dropdown 1</a></li>
+              <li><a class="dropdown-item" href="#">Deep Dropdown</a></li>
+              <li><a class="dropdown-item" href="#">Dropdown 2</a></li>
+              <li><a class="dropdown-item" href="#">Dropdown 3</a></li>
+              <li><a class="dropdown-item" href="#">Dropdown 4</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Contact</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
